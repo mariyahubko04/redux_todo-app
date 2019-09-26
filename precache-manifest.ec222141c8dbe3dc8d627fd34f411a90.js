fcfea0906ed3f651b8eef9e91de03f2b70a65700
unknown <mariia>
2019-09-26 15:51:49 +0300

    Updates

---
+++ precache-manifest.ec222141c8dbe3dc8d627fd34f411a90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7a13a9ff82f85b2e191aaff42b5d57",
+    "url": "/redux_todo-app/index.html"
+  },
+  {
+    "revision": "9f4b9227b1f3cbc70aeb",
+    "url": "/redux_todo-app/static/css/main.27eab42d.chunk.css"
+  },
+  {
+    "revision": "e34a1b0a82d0d350b8e1",
+    "url": "/redux_todo-app/static/js/2.68a0c1fd.chunk.js"
+  },
+  {
+    "revision": "9f4b9227b1f3cbc70aeb",
+    "url": "/redux_todo-app/static/js/main.bceed936.chunk.js"
+  },
+  {
+    "revision": "f6237b85156e86845fdb",
+    "url": "/redux_todo-app/static/js/runtime~main.dc666ae1.js"
+  }
+]);
